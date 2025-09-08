235f12de919e7d423b31bea08941bd65be42e315
angoroa <aaa000117@naver.com>
2025-09-08 09:17:01 +0900

    :tada:Start: init repository setting

---
+++ src/routes/Router.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import MainPage from '../pages/MainPage'
+
+export default function AppRouter() {
+  return (
+    <Router>
+        <Routes>
+            <Route path="/" element={<MainPage />} />
+        </Routes>
+    </Router>
+  )
+}
