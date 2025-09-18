5891849a94e6b61112f87c9e06d46fc096368022
wnsgur393 <2021301022@skuniv.ac.kr>
2025-09-18 13:35:22 +0900

    🔧 Settings: Vite path alias

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@apis", replacement: "/src/apis" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@images", replacement: "/src/assets/images" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@commons", replacement: "/src/components/commons" },
+      { find: "@layout", replacement: "/src/components/layout" },
+      { find: "@data", replacement: "/src/data" },
+      { find: "@hooks", replacement: "/src/hooks" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@utils", replacement: "/src/utils" },
+    ],
+  }
 })
