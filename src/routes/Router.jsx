4b2f3c597ace3b530bf6a67117eacc5f3a09affa
wnsgur393 <2021301022@skuniv.ac.kr>
2025-09-20 17:06:14 +0900

    ✨ Feat : Footer

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import RootLayout from '@components/layout/RootLayout'
+
 import MainPage from '@pages/MainPage'
+import MapPage from '@pages/MapPage'
+import CartPage from '@pages/CartPage'
+import ProfilePage from '@pages/ProfilePage'
+
 export default function AppRouter() {
   return (
     <Router>
         <Routes>
-            <Route path="/" element={<MainPage />} />
+            <Route element={<RootLayout />}>
+              <Route index element={<MainPage />} />
+              <Route path="/map" element={<MapPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+            </Route>
         </Routes>
     </Router>
-  )
+  );
 }
